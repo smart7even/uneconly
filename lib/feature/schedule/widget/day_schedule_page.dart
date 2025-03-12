7b0266b897189c097a67a8e090aa15a79486fbde
Oleg <upliftingpark@gmail.com>
2025-03-13 01:14:43 +0300

    fix add no lessons placeholder back

--- lib/feature/schedule/widget/day_schedule_page.dart
+++ lib/feature/schedule/widget/day_schedule_page.dart
@@ -44,68 +44,75 @@ class DaySchedulePage extends StatelessWidget {
           ),
         ),
       ),
-      body: CustomScrollView(
-        slivers: [
-          // if (daySchedule.lessons.isEmpty)
-          //   const Expanded(
-          //     child: Center(
-          //       child: Padding(
-          //         padding: EdgeInsets.all(8),
-          //         child: Text(
-          //           'На этот день нет пар 🍀',
-          //           style: TextStyle(
-          //             fontWeight: FontWeight.bold,
-          //             fontSize: 18,
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          if (grouppedLessonsByTime.isNotEmpty)
-            SliverToBoxAdapter(
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(8),
-                child: Text(
-                  'Всего пар: ${grouppedLessonsByTime.length}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return CustomScrollView(
+            slivers: [
+              if (daySchedule.lessons.isEmpty)
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: constraints.maxHeight,
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          'На этот день нет пар 🍀',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  textAlign: TextAlign.start,
                 ),
-              ),
-            ),
-          // if (overlappingLessons.isNotEmpty)
-          //   Padding(
-          //     padding: const EdgeInsets.all(8),
-          //     child: Text(
-          //       'Внимание! На это время назначено несколько пар',
-          //       style: TextStyle(
-          //         color: Theme.of(context).colorScheme.error,
-          //       ),
-          //     ),
-          //   ),
-          if (daySchedule.lessons.isNotEmpty)
-            SliverList.builder(
-              itemCount: daySchedule.lessons.length,
-              itemBuilder: (context, index) {
-                return LessonBigTile(
-                  lesson: daySchedule.lessons[index],
-                  lessonNumber: getLessonNumber(
-                    grouppedLessonsByTime,
-                    daySchedule.lessons[index],
+              if (grouppedLessonsByTime.isNotEmpty)
+                SliverToBoxAdapter(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      'Всего пар: ${grouppedLessonsByTime.length}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
                   ),
-                );
-              },
-            ),
-          // SliverToBoxAdapter(
-          //   child: UneconlyDivKitView(
-          //     data: {}
-          //     ),
-          //   ),
-          // ),
-        ],
+                ),
+              // if (overlappingLessons.isNotEmpty)
+              //   Padding(
+              //     padding: const EdgeInsets.all(8),
+              //     child: Text(
+              //       'Внимание! На это время назначено несколько пар',
+              //       style: TextStyle(
+              //         color: Theme.of(context).colorScheme.error,
+              //       ),
+              //     ),
+              //   ),
+              if (daySchedule.lessons.isNotEmpty)
+                SliverList.builder(
+                  itemCount: daySchedule.lessons.length,
+                  itemBuilder: (context, index) {
+                    return LessonBigTile(
+                      lesson: daySchedule.lessons[index],
+                      lessonNumber: getLessonNumber(
+                        grouppedLessonsByTime,
+                        daySchedule.lessons[index],
+                      ),
+                    );
+                  },
+                ),
+              // SliverToBoxAdapter(
+              //   child: UneconlyDivKitView(
+              //     data: {}
+              //     ),
+              //   ),
+              // ),
+            ],
+          );
+        },
       ),
     );
   }
